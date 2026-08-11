d6190eb0079efdaf1f21f6a010ff6dc43fcd7249
HwangYHa <89735480+HwangYHa@users.noreply.github.com>
2026-08-11 16:17:37 +0900

    enable complete Korean control patch

--- gui/__init__.py
+++ gui/__init__.py
@@ -1,6 +1,8 @@
 """오토셀러AI 사용자 화면 패키지."""
 from gui.korean_runtime import apply_korean_patch
+from gui.korean_runtime_extra import apply_extra_korean_patch
 from gui.pageconfig_ko import patch_page_config
 
 apply_korean_patch()
+apply_extra_korean_patch()
 patch_page_config()
